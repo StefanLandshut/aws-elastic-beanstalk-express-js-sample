45a5147acafa7340d77022531eb4d76521601c27
StefanLandshut <160515380+StefanLandshut@users.noreply.github.com>
2024-09-04 19:04:35 -0400

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World ;D!'));
+app.get('/', (req, res) => res.send('HOLA PERSONAS DE SONDA, SOY STEFAN Y TENGO MUCHO SUEÑO!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
